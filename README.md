e7ecf87abbe1507c5b1e577f342fc05365014c3a
trancore <ki.program0915@gmail.com>
2023-10-01 14:19:02 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Todo RESTful API
---
+++ src/test/kotlin/com/trancore/todoapi/TodoapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.trancore.todoapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodoapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

18eb2d4ad9f471e004b921ba657834b9debcc5ce
trancore <ki.program0915@gmail.com>
2023-10-21 22:53:49 +0900

    feature: implement todos API outside

---
+++ src/controllers/todoController.ts
@@ -0,0 +1,19 @@
+﻿import express from 'express';
+
+const app = express;
+
+export const todoController = app.Router();
+
+/**
+ * TODO-001 Todo一覧取得
+ */
+todoController.get('/todos', (req, res) => {
+  console.log('GET /todos');
+});
+
+/**
+ * TODO-003 Todo作成
+ */
+todoController.post('/todos', (req, res) => {
+  console.log('GET /todos');
+});
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 ﻿import express from 'express';
+
+import { todoController } from './controllers/todoController';
+
 import { CONFIG } from './configrations/config';
 
 const app = express();
+
+// controller
+app.use('/api', todoController);
 
 app.listen(CONFIG.PORT, () => {
   console.log('server start.');

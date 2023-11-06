1888f02d9910fc092abad2f089d108ce0a127039
trancore <ki.program0915@gmail.com>
2023-11-06 23:05:50 +0900

    feature: add tokenRepository

---
+++ src/repositories/tokenRepository.ts
@@ -0,0 +1,33 @@
+﻿import { PrismaClient } from '@prisma/client';
+
+import { TokenData } from '../types/authentication';
+
+export const tokenRepository = async () => {
+  const prisma = new PrismaClient();
+
+  const saveToken = async (
+    userId: number,
+    { accessToken, refreshToken }: TokenData,
+  ) => {
+    try {
+      await prisma.token.upsert({
+        where: {
+          id: userId,
+        },
+        update: {
+          accessToken: accessToken,
+          refreshToken: refreshToken || 'hoge',
+        },
+        create: {
+          user_id: userId,
+          accessToken: accessToken,
+          refreshToken: refreshToken || 'hoge',
+        },
+      });
+    } catch (error) {
+      // TODO 一旦無視
+    }
+  };
+
+  return { saveToken };
+};

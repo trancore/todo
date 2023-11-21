40ab077096fd09e1c73d4054804b7597ee13b991
trancore <ki.program0915@gmail.com>
2023-11-21 22:51:09 +0900

    feature: add logger libs

---
+++ src/libs/logger.ts
@@ -0,0 +1,17 @@
+﻿import { ErrorRequestHandler } from 'express';
+import log4js from 'log4js';
+
+import { loggerConfig } from '../configurations/logger';
+
+log4js.configure(loggerConfig);
+
+export const console = log4js.getLogger();
+export const system = log4js.getLogger('system');
+
+/** システムロガーモジュール */
+export const systemLogger =
+  (options = {}): ErrorRequestHandler =>
+  (err, req, res, next) => {
+    system.error(err.message);
+    next(err);
+  };

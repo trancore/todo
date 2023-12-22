631025aea1e0d18ef5e646a528d4246d45c411b1
trancore <ki.program0915@gmail.com>
2023-12-23 01:29:06 +0900

    feature: add storybook

--- apps/front/todo-nextjs/src/app/page.tsx
+++ apps/front/todo-nextjs/src/app/page.tsx
@@ -1,8 +1,14 @@
+import ButtonContainer from '~/components/Button/ButtonContainer';
+
 export default function Home() {
   return (
     <main>
       <div>
         <p>Hello, Next.js</p>
+        <ButtonContainer
+          test="test"
+          presentational={{ text: 'text' }}
+        ></ButtonContainer>
       </div>
     </main>
   );

0e8cd4b6c9cecdfefd94bfbf7d0012b26813814c
trancore <ki.program0915@gmail.com>
2023-12-24 22:24:47 +0900

    feature: set storybook components setting

--- apps/front/todo-nextjs/.storybook/main.ts
+++ apps/front/todo-nextjs/.storybook/main.ts
@@ -1,7 +1,8 @@
 import type { StorybookConfig } from '@storybook/nextjs';
+import path from 'path';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(tsx)'],
+  stories: ['../src/**/*.stories.(tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
@@ -14,6 +15,15 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: 'tag',
+  },
+  webpackFinal: (config) => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve?.alias,
+        '~': [path.resolve(__dirname, '../src/')],
+      };
+    }
+    return config;
   },
 };
 export default config;

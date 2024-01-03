e119201c35ed6699ee1c9826939d2f50f2bf1c31
trancore <ki.program0915@gmail.com>
2024-01-03 23:18:19 +0900

    feature: implement Icon component

---
+++ apps/front/todo-nextjs/src/components/container/Icon/Icon.tsx
@@ -0,0 +1,11 @@
+﻿import { ComponentProps } from 'react';
+
+import IconPresentational from '~/components/presentational/Icon/Icon';
+
+type Props = {
+  presentational: ComponentProps<typeof IconPresentational>;
+};
+
+export default function Icon({ presentational }: Props) {
+  return <IconPresentational {...presentational} />;
+}

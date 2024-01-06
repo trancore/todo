6b74c542c30207d52e94d79d1fb92a1f10323a97
trancore <ki.program0915@gmail.com>
2024-01-06 22:26:31 +0900

    feature: add reset css

--- apps/front/todo-nextjs/src/app/layout.tsx
+++ apps/front/todo-nextjs/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
+import '/public/css/reset.css';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {

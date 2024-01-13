4e551ad44c3f2dd69b645b8708246763e8232d73
trancore <ki.program0915@gmail.com>
2024-01-13 22:48:21 +0900

    feature: implement Modal component

---
+++ apps/front/todo-nextjs/src/components/container/Modal/Modal.tsx
@@ -0,0 +1,11 @@
+﻿'use client';
+
+import ModalPresentational from '~/components/presentational/Modal/Modal';
+
+type Props = {
+  children: JSX.Element;
+};
+
+export default function Form({ children }: Props) {
+  return <ModalPresentational>{children}</ModalPresentational>;
+}
---
+++ apps/front/todo-nextjs/src/components/presentational/Modal/Modal.tsx
@@ -0,0 +1,33 @@
+﻿'use client';
+
+import styled from 'styled-components';
+
+type Props = {
+  children: JSX.Element;
+};
+
+const StyledBackFloat = styled.div`
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+const StyledModalContent = styled.div`
+  width: 85%;
+  height: 85%;
+  background-color: white;
+`;
+
+export default function Modal({ children }: Props) {
+  return (
+    <StyledBackFloat>
+      <StyledModalContent>{children}</StyledModalContent>
+    </StyledBackFloat>
+  );
+}

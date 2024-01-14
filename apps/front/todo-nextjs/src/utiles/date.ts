7996a770b8ad8deb585663cbb979c03ac88dd7d5
trancore <ki.program0915@gmail.com>
2024-01-14 22:11:41 +0900

    feature: add date format in utilities

---
+++ apps/front/todo-nextjs/src/utiles/date.ts
@@ -0,0 +1,19 @@
+﻿/**
+ * 日付変換用ユーティーリティー
+ */
+export function dateFormat() {
+  /**
+   * Date型をYYYY/MM/ddの形に変換する
+   * @param date Date型日付
+   * @returns YYYY/MM/dd
+   */
+  function formatToYYYYMMdd(date: Date) {
+    return date.toLocaleDateString('ja-JP', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+    });
+  }
+
+  return { formatToYYYYMMdd };
+}

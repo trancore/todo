921e2078c5e586b734c6daf08443fb04fea45d0d
trancore <ki.program0915@gmail.com>
2024-01-14 22:10:32 +0900

    feature: update styled component property in Button component

--- apps/front/todo-nextjs/src/components/presentational/Button/Button.tsx
+++ apps/front/todo-nextjs/src/components/presentational/Button/Button.tsx
@@ -5,18 +5,18 @@ type Props = {
   width?: number;
 };
 
-export default function Button({ text, width }: Props) {
-  const Button = styled.button<{ width?: number }>`
-    border: none;
-    border-radius: 9em;
-    padding: 16px 12px;
-    width: ${(props) => {
-      return props.width && `${props.width}px`;
-    }};
-    color: #ffffff;
-    // TODO: storeでテーマ管理したい
-    background-color: #8f8f8f;
-  `;
+const StyledButton = styled.button<{ width?: number }>`
+  border: none;
+  border-radius: 9em;
+  padding: 16px 12px;
+  width: ${(props) => {
+    return props.width && `${props.width}px`;
+  }};
+  color: #ffffff;
+  // TODO: storeでテーマ管理したい
+  background-color: #8f8f8f;
+`;
 
-  return <Button width={width}>{text}</Button>;
+export default function Button({ text, width }: Props) {
+  return <StyledButton width={width}>{text}</StyledButton>;
 }

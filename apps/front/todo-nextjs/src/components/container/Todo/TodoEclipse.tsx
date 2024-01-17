b80c16ca214484f152d14b776703e712fd4484b4
trancore <ki.program0915@gmail.com>
2024-01-17 23:58:19 +0900

    feature: implement TodoEclipse component

---
+++ apps/front/todo-nextjs/src/components/container/Todo/TodoEclipse.tsx
@@ -0,0 +1,25 @@
+﻿import { ComponentProps } from 'react';
+
+import TextEclipsePresentation from '~/components/presentational/Todo/TodoEclipse';
+
+type Props = {
+  presentational: Omit<ComponentProps<typeof TextEclipsePresentation>, 'click'>;
+  click: () => undefined;
+};
+
+export default function TodoEclipse({ presentational }: Props) {
+  const { title, description } = presentational;
+
+  // TODO ロジックを実装する
+  function openModal() {
+    return undefined;
+  }
+
+  return (
+    <TextEclipsePresentation
+      title={title}
+      description={description}
+      click={openModal}
+    />
+  );
+}

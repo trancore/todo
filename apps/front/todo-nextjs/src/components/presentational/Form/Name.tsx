7c11c554bb45f31e1b6a7da6192e1a0f45f6f9ef
trancore <ki.program0915@gmail.com>
2024-02-03 01:10:06 +0900

    feature: implement Name form component

---
+++ apps/front/todo-nextjs/src/components/presentational/Form/Name.tsx
@@ -0,0 +1,30 @@
+﻿'use client';
+
+import { UseFormRegisterReturn } from 'react-hook-form';
+import styled from 'styled-components';
+
+type Props = {
+  labelName: string;
+  placeholder?: string;
+  errorMessage: string | undefined;
+  register: UseFormRegisterReturn | undefined;
+};
+
+const StyledInput = styled.input`
+  border-radius: 50px;
+`;
+
+export default function Name({
+  labelName,
+  placeholder,
+  errorMessage,
+  register,
+}: Props) {
+  return (
+    <>
+      <label>{labelName}</label>
+      <StyledInput placeholder={placeholder} {...register} />
+      <p>{errorMessage}</p>
+    </>
+  );
+}

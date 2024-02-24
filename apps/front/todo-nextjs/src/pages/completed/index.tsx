a49c759d079cbccc710a9d65e44c39c340e303ee
trancore <ki.program0915@gmail.com>
2024-02-24 23:47:45 +0900

    feature: implement completed page

---
+++ apps/front/todo-nextjs/src/pages/completed/index.tsx
@@ -0,0 +1,74 @@
+﻿import styled from 'styled-components';
+import TodoEclipse from '~/components/container/Todo/TodoEclipse';
+import TodoIconBox from '~/components/container/Todo/TodoIconBox';
+
+const StyledTodoList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 48px;
+`;
+const StyledTodo = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const StyledTodoUnder = styled.div`
+  margin-top: 8px;
+  padding: 0 12px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export default function Completed() {
+  return (
+    <>
+      <h1>完了済み</h1>
+      <StyledTodoList>
+        <StyledTodo>
+          <TodoEclipse
+            presentational={{ title: 'test', description: 'description' }}
+          />
+          <StyledTodoUnder>
+            <p>2024/01/01</p>
+            <TodoIconBox />
+          </StyledTodoUnder>
+        </StyledTodo>
+        <StyledTodo>
+          <TodoEclipse
+            presentational={{ title: 'test', description: 'description' }}
+          />
+          <StyledTodoUnder>
+            <p>2024/01/01</p>
+            <TodoIconBox />
+          </StyledTodoUnder>
+        </StyledTodo>
+        <StyledTodo>
+          <TodoEclipse
+            presentational={{ title: 'test', description: 'description' }}
+          />
+          <StyledTodoUnder>
+            <p>2024/01/01</p>
+            <TodoIconBox />
+          </StyledTodoUnder>
+        </StyledTodo>
+        <StyledTodo>
+          <TodoEclipse
+            presentational={{ title: 'test', description: 'description' }}
+          />
+          <StyledTodoUnder>
+            <p>2024/01/01</p>
+            <TodoIconBox />
+          </StyledTodoUnder>
+        </StyledTodo>
+        <StyledTodo>
+          <TodoEclipse
+            presentational={{ title: 'test', description: 'description' }}
+          />
+          <StyledTodoUnder>
+            <p>2024/01/01</p>
+            <TodoIconBox />
+          </StyledTodoUnder>
+        </StyledTodo>
+      </StyledTodoList>
+    </>
+  );
+}

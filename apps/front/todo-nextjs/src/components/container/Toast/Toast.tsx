e8d160c397d9bb2ddd58684fdaa4f2aeb73059e4
trancore <ki.program0915@gmail.com>
2024-03-13 22:52:33 +0900

    feature: add Toast component

---
+++ apps/front/todo-nextjs/src/components/container/Toast/Toast.tsx
@@ -0,0 +1,13 @@
+﻿'use client';
+
+import { ComponentProps } from 'react';
+
+import ToastPresentational from '~/components/presentational/Toast/Toast';
+
+type Props = {
+  presentational: ComponentProps<typeof ToastPresentational>;
+};
+
+export default function Toast({ presentational }: Props) {
+  return <ToastPresentational text={presentational.text} />;
+}
---
+++ apps/front/todo-nextjs/src/components/presentational/Toast/Toast.tsx
@@ -0,0 +1,22 @@
+﻿'use client';
+
+import styled from 'styled-components';
+
+type Props = {
+  text: string;
+};
+
+const StyledToast = styled.div`
+  padding: 0 32px;
+  width: fit-content;
+  border: 2px solid red;
+  font-weight: bold;
+`;
+
+export default function Modal({ text }: Props) {
+  return (
+    <StyledToast>
+      <p>{text}</p>
+    </StyledToast>
+  );
+}

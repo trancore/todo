66a0c1d8f4eec20fbfaab6aa92850a85251f9128
trancore <ki.program0915@gmail.com>
2024-03-16 00:33:11 +0900

    feature: feature toast design and animation

--- apps/front/todo-nextjs/src/components/presentational/Toast/Toast.tsx
+++ apps/front/todo-nextjs/src/components/presentational/Toast/Toast.tsx
@@ -2,21 +2,34 @@
 
 import styled from 'styled-components';
 
+import { popup } from '~/libs/styledComponent';
+
 type Props = {
   text: string;
+  displayed: boolean;
 };
 
 const StyledToast = styled.div`
-  padding: 0 32px;
+  position: fixed;
+  margin: 0 auto;
+  left: 0;
+  right: 0;
+  bottom: 24px;
   width: fit-content;
+  padding: 0 32px;
   border: 2px solid red;
+  background-color: white;
   font-weight: bold;
+
+  animation: ${popup} 2s cubic-bezier(0.33, 1, 0.68, 1) forwards;
 `;
 
-export default function Modal({ text }: Props) {
+export default function Toast({ text, displayed }: Props) {
   return (
-    <StyledToast>
-      <p>{text}</p>
-    </StyledToast>
+    displayed && (
+      <StyledToast>
+        <p>{text}</p>
+      </StyledToast>
+    )
   );
 }

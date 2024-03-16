c575ced38ad1c2924c4b43a6c39660b894cf6883
trancore <ki.program0915@gmail.com>
2024-03-16 16:13:15 +0900

    feature: implement Error component

---
+++ apps/front/todo-nextjs/src/components/container/Error/Error.tsx
@@ -0,0 +1,13 @@
+﻿import { ComponentProps } from 'react';
+
+import ErrorPresentation from '~/components/presentational/Error/Error';
+
+type Props = {
+  presentational: ComponentProps<typeof ErrorPresentation>;
+};
+
+export default function Error({ presentational }: Props) {
+  const { text } = presentational;
+
+  return <ErrorPresentation text={text} />;
+}
---
+++ apps/front/todo-nextjs/src/components/presentational/Error/Error.tsx
@@ -0,0 +1,15 @@
+﻿import { styled } from 'styled-components';
+
+type Props = {
+  text?: string;
+};
+
+const StyledErrorparagraph = styled.p`
+  padding: 24px 24px;
+  background-color: lightcoral;
+  color: crimson;
+`;
+
+export default function Error({ text }: Props) {
+  return <StyledErrorparagraph>{text}</StyledErrorparagraph>;
+}

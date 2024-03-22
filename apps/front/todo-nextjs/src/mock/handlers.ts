3c9a65f529f1ddd5702931a0e7fe97edf2111966
trancore <ki.program0915@gmail.com>
2024-03-23 02:33:01 +0900

    refactor

--- apps/front/todo-nextjs/src/mock/handlers.ts
+++ apps/front/todo-nextjs/src/mock/handlers.ts
@@ -1,16 +1,7 @@
 ﻿import { HttpResponse, http } from 'msw';
-import { paths } from '~/types/openapi';
-
-type PostTodoRequest =
-  paths['/todos']['post']['requestBody']['content']['application/json'];
-type PostTodoResponse = undefined;
 
 export const handlers = [
-  http.post(
-    `${process.env.TODO_API_URL}/todos`,
-    async ({ request, params, cookies }) => {
-      console.log('POST: /todos req:', request.json());
-      return new HttpResponse(undefined, { status: 201 });
-    },
-  ),
+  http.post(`${process.env.TODO_API_URL}/todos`, async () => {
+    return new HttpResponse(undefined, { status: 201 });
+  }),
 ];

bb8d26fba53650b4614f2bb525c1f34b30b2f330
trancore <ki.program0915@gmail.com>
2024-03-24 01:49:24 +0900

    feature: implement GET /todos

--- apps/front/todo-nextjs/src/pages/index.tsx
+++ apps/front/todo-nextjs/src/pages/index.tsx
@@ -1,6 +1,17 @@
 ﻿import styled from 'styled-components';
+
+import { wrapper } from '~/store/root';
+
 import TodoEclipse from '~/components/container/Todo/TodoEclipse';
 import TodoIconBox from '~/components/container/Todo/TodoIconBox';
+
+import {
+  getRunningQueriesThunk,
+  getTodos,
+  useGetTodosQuery,
+} from '~/services/todo';
+
+import { dateFormat } from '~/utils/date';
 
 const StyledTodoList = styled.div`
   display: flex;
@@ -19,53 +30,37 @@ const StyledTodoUnder = styled.div`
 `;
 
 export default function Top() {
+  const { data: todoList } = useGetTodosQuery();
+
+  const { formatToYYYYMMdd } = dateFormat();
+
   return (
     <StyledTodoList>
-      <StyledTodo>
-        <TodoEclipse
-          presentational={{ title: 'test', description: 'description' }}
-        />
-        <StyledTodoUnder>
-          <p>2024/01/01</p>
-          <TodoIconBox />
-        </StyledTodoUnder>
-      </StyledTodo>
-      <StyledTodo>
-        <TodoEclipse
-          presentational={{ title: 'test', description: 'description' }}
-        />
-        <StyledTodoUnder>
-          <p>2024/01/01</p>
-          <TodoIconBox />
-        </StyledTodoUnder>
-      </StyledTodo>
-      <StyledTodo>
-        <TodoEclipse
-          presentational={{ title: 'test', description: 'description' }}
-        />
-        <StyledTodoUnder>
-          <p>2024/01/01</p>
-          <TodoIconBox />
-        </StyledTodoUnder>
-      </StyledTodo>
-      <StyledTodo>
-        <TodoEclipse
-          presentational={{ title: 'test', description: 'description' }}
-        />
-        <StyledTodoUnder>
-          <p>2024/01/01</p>
-          <TodoIconBox />
-        </StyledTodoUnder>
-      </StyledTodo>
-      <StyledTodo>
-        <TodoEclipse
-          presentational={{ title: 'test', description: 'description' }}
-        />
-        <StyledTodoUnder>
-          <p>2024/01/01</p>
-          <TodoIconBox />
-        </StyledTodoUnder>
-      </StyledTodo>
+      {(todoList ?? []).map((todo) => (
+        <StyledTodo key={todo.id}>
+          <TodoEclipse
+            presentational={{
+              title: todo.title,
+              description: todo.description || '',
+            }}
+          />
+          <StyledTodoUnder>
+            {todo.deadlineAt && (
+              <p>{formatToYYYYMMdd(new Date(todo.deadlineAt))}</p>
+            )}
+            <TodoIconBox />
+          </StyledTodoUnder>
+        </StyledTodo>
+      ))}
     </StyledTodoList>
   );
 }
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) => async () => {
+    store.dispatch(getTodos.initiate());
+    await Promise.all(store.dispatch(getRunningQueriesThunk()));
+
+    return { props: {} };
+  },
+);

65ea293918fa646137cc72ad20bebfd25875cb66
trancore <ki.program0915@gmail.com>
2024-03-26 23:34:33 +0900

    feature: implement todoModal hooks

---
+++ apps/front/todo-nextjs/src/hooks/useTodoModal.ts
@@ -0,0 +1,23 @@
+﻿import { TodoForm } from '~/types/todo';
+
+import { selectTodoModal } from '~/features/todoModal';
+import { close, open } from '~/features/todoModal';
+
+import { useAppDispatch, useAppSelector } from '~/hooks/useRedux';
+
+export const useTodoModal = () => {
+  const store = useAppSelector(selectTodoModal);
+  const dispatch = useAppDispatch();
+
+  const isOpendModal = store.displayed;
+
+  const openTodoModal = (todo: TodoForm) => {
+    dispatch(open(todo));
+  };
+
+  const closeTodoModal = () => {
+    dispatch(close());
+  };
+
+  return { isOpendModal, openTodoModal, closeTodoModal };
+};

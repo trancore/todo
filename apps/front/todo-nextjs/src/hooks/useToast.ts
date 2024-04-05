bd0a9115137d0f62581e194c0a242abff87b7027
trancore <ki.program0915@gmail.com>
2024-04-06 01:02:16 +0900

    feature: rename

--- apps/front/todo-nextjs/src/hooks/useToast.ts
+++ apps/front/todo-nextjs/src/hooks/useToast.ts
@@ -2,14 +2,14 @@
 
 import { useAppDispatch } from '~/hooks/useRedux';
 
-const DELAY_TIME = 2000;
+const DELAY_TIME_SEC = 2000;
 
 export const useToast = () => {
   const dispatch = useAppDispatch();
 
   function hookToast(text: string) {
     dispatch(showTodo({ text: text }));
-    const timeoutId = setTimeout(() => dispatch(hideTodo()), DELAY_TIME);
+    const timeoutId = setTimeout(() => dispatch(hideTodo()), DELAY_TIME_SEC);
     clearTimeout(timeoutId);
   }
 

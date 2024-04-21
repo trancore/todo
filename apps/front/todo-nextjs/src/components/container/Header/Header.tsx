3ecf048363c0cbe83a70b0d8c7ce53afd5acc1af
trancore <ki.program0915@gmail.com>
2024-04-21 21:44:00 +0900

    feature: implement register link

--- apps/front/todo-nextjs/src/components/container/Header/Header.tsx
+++ apps/front/todo-nextjs/src/components/container/Header/Header.tsx
@@ -1,8 +1,28 @@
-﻿import HeaderPresentational from '~/components/presentational/Header/Header';
+﻿import { useRouter as useRouterNav } from 'next/navigation';
+import { useRouter } from 'next/router';
+
+import HeaderPresentational from '~/components/presentational/Header/Header';
+
+import { PAGE_PATH } from '~/constants';
 
 export default function Header() {
-  // TODO サインイン、サインアウト処理ができてから対応する。
-  const isSignin = false;
+  const { push } = useRouterNav();
+  const { pathname } = useRouter();
 
-  return <HeaderPresentational isSignin={isSignin} />;
+  // TODO サインイン、サインアウト処理ができてから対応する。
+  const isSignin = true;
+
+  const hasPlusIcon = pathname !== PAGE_PATH.REGISTER;
+
+  function onClickPlusIcon() {
+    push(PAGE_PATH.REGISTER);
+  }
+
+  return (
+    <HeaderPresentational
+      isSignin={isSignin}
+      hasPlusIcon={hasPlusIcon}
+      onClickPlusIcon={onClickPlusIcon}
+    />
+  );
 }

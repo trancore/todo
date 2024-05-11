0d8e31b657cc3702ad54ed87bfc6cf7834ae3f19
trancore <ki.program0915@gmail.com>
2024-05-11 23:16:52 +0900

    feature: aggregate reducers

--- apps/front/todo-nextjs/src/store/root.ts
+++ apps/front/todo-nextjs/src/store/root.ts
@@ -1,4 +1,4 @@
-﻿import { configureStore } from '@reduxjs/toolkit';
+﻿import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query/react';
 import { createWrapper } from 'next-redux-wrapper';
 import errorReducer from '~/state/error';
@@ -10,21 +10,28 @@ import todoReducer from '~/state/todo';
 import { todoApi } from '~/services/todo';
 
 export type AppStore = typeof rootStore;
-export type RootState = ReturnType<typeof rootStore.getState>;
+export type RootState = ReturnType<typeof rootReducer>;
 export type AppDispatch = typeof rootStore.dispatch;
 
-export const rootStore = configureStore({
-  reducer: {
-    error: errorReducer,
-    menu: menuReducer,
-    modal: modalReducer,
-    toast: toastReducer,
-    todo: todoReducer,
-    [todoApi.reducerPath]: todoApi.reducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(todoApi.middleware),
+const rootReducer = combineReducers({
+  error: errorReducer,
+  menu: menuReducer,
+  modal: modalReducer,
+  toast: toastReducer,
+  todo: todoReducer,
+  [todoApi.reducerPath]: todoApi.reducer,
 });
+
+export const setupStore = (preloadedState?: Partial<RootState>) => {
+  return configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(todoApi.middleware),
+    preloadedState,
+  });
+};
+
+export const rootStore = setupStore();
 
 export const wrapper = createWrapper<AppStore>(() => rootStore);
 

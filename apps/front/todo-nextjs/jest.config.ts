368b6c1414a66001ef59e0c180874a06b0b115c7
trancore <ki.program0915@gmail.com>
2024-05-15 23:43:16 +0900

    feature: implement API server setting for test

--- apps/front/todo-nextjs/jest.config.ts
+++ apps/front/todo-nextjs/jest.config.ts
@@ -5,6 +5,7 @@
 import nextJest from 'next/jest.js';
 
 import type { Config } from 'jest';
+import util from 'util';
 
 const createJestConfig = nextJest({
   dir: './',
@@ -72,7 +73,10 @@ const config: Config = {
   // globalTeardown: undefined,
 
   // A set of global variables that need to be available in all test environments
-  // globals: {},
+  globals: {
+    TextEncoder: util.TextEncoder,
+    TextDecoder: util.TextDecoder,
+  },
 
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
   // maxWorkers: "50%",
---
+++ apps/front/todo-nextjs/test/__mocks__/setup.ts
@@ -0,0 +1,18 @@
+﻿import { rootStore } from '~/store/root';
+
+import { todoApi as todoApiSlice } from '~/services/todo';
+
+import { server } from '@/__mocks__/server';
+
+const store = rootStore;
+
+beforeAll(() => {
+  server.listen();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+  store.dispatch(todoApiSlice.util.resetApiState());
+});
+
+afterAll(() => server.close());

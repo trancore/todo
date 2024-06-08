a2671ea4f9c4fc337afd0aa4bdc70c06fcb60ff2
trancore <ki.program0915@gmail.com>
2024-06-08 22:11:57 +0900

    feature: implement Header component test

--- apps/front/todo-nextjs/test/__tests__/component/Header/Header.test.tsx
+++ apps/front/todo-nextjs/test/__tests__/component/Header/Header.test.tsx
@@ -1,4 +1,5 @@
-﻿import { cleanup, render } from '@testing-library/react';
+﻿import { cleanup, render, waitFor } from '@testing-library/react';
+import userEvent, { UserEvent } from '@testing-library/user-event';
 
 import Header from '~/components/container/Header/Header';
 
@@ -10,7 +11,7 @@ const mockOpen = jest.fn();
 const mockUseAppDispatch = jest.fn();
 const mockIsAuth = jest.fn();
 const mockUseRouterNav = jest.fn().mockImplementation(() => ({
-  push: mockPush(),
+  push: (...arg: unknown[]) => mockPush(...arg),
 }));
 const mockUseRouter = jest.fn().mockImplementation(() => ({
   pathname: mockPathname(),
@@ -29,17 +30,20 @@ jest.mock('~/state/menu', () => ({
   open: () => mockOpen(),
 }));
 jest.mock('~/hooks/useRedux', () => ({
-  useAppDispatch: () => mockUseAppDispatch(),
+  useAppDispatch: () => () => mockUseAppDispatch(),
 }));
 jest.mock('~/hooks/useSignin', () => ({
   useSignin: () => mockUseSignin(),
 }));
 
 describe('~/components/container/Header/Header.tsx', () => {
+  let user: UserEvent;
+
   beforeEach(() => {
     cleanup();
     mockIsAuth.mockReset();
     mockPathname.mockReset();
+    user = userEvent.setup();
   });
 
   it('サインインしていない', () => {
@@ -75,5 +79,54 @@ describe('~/components/container/Header/Header.tsx', () => {
     expect(plusElement).toHaveLength(1);
   });
 
-  // ユーザーイベント
+  it('メニューをクリックして、メニューモーダルを表示する', async () => {
+    mockIsAuth.mockReturnValue(true);
+    mockPathname.mockReturnValue(PAGE_PATH.TOP);
+
+    const { container } = render(<Header />);
+    const menuElement = container.querySelector('#Menu');
+
+    expect(menuElement).toBeDefined();
+
+    await user.click(menuElement!);
+
+    await waitFor(() => {
+      expect(mockUseAppDispatch).toHaveBeenCalled();
+      expect(mockOpen).toHaveBeenCalled();
+    });
+  });
+
+  it('プラスアイコンをクリックして、TODO登録画面に遷移する', async () => {
+    mockIsAuth.mockReturnValue(true);
+    mockPathname.mockReturnValue(PAGE_PATH.TOP);
+
+    const { container } = render(<Header />);
+    const plusElement = container.querySelector('#Plus');
+
+    expect(plusElement).toBeDefined();
+
+    await user.click(plusElement!);
+
+    await waitFor(() => {
+      expect(mockPush).toHaveBeenCalled();
+      expect(mockPush).toHaveBeenCalledWith(PAGE_PATH.REGISTER);
+    });
+  });
+
+  it('ユーザーアイコンをクリックして、トップ画面に遷移する', async () => {
+    mockIsAuth.mockReturnValue(true);
+    mockPathname.mockReturnValue(PAGE_PATH.COMPLETED);
+
+    const { container } = render(<Header />);
+    const userElement = container.querySelector('#UserCircle');
+
+    expect(userElement).toBeDefined();
+
+    await user.click(userElement!);
+
+    await waitFor(() => {
+      expect(mockPush).toHaveBeenCalled();
+      expect(mockPush).toHaveBeenCalledWith(PAGE_PATH.TOP);
+    });
+  });
 });

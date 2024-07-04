4e64e28dc42f3b2ef70dc499041a5e5bbff32887
trancore <ki.program0915@gmail.com>
2024-07-04 23:03:52 +0900

    feature: implement test in Toast component

---
+++ apps/front/todo-nextjs/test/__tests__/component/Toast/Toast.test.tsx
@@ -0,0 +1,46 @@
+﻿import { cleanup, render } from '@testing-library/react';
+
+import Toast from '~/components/container/Toast/Toast';
+
+const mockUseAppSelector = jest.fn();
+const mockSelectToast = jest.fn();
+
+jest.mock('~/state/toast', () => ({
+  selectToast: () => mockSelectToast(),
+}));
+jest.mock('~/hooks/useRedux', () => ({
+  useAppSelector: (...arg: unknown[]) => mockUseAppSelector(...arg),
+}));
+
+describe('~/component/container/Toast/Toast.tsx', () => {
+  beforeEach(() => {
+    mockUseAppSelector.mockReset();
+    cleanup();
+  });
+
+  it('トーストを表示する。', async () => {
+    const mockStore = {
+      displayed: true,
+      text: 'test',
+    };
+    mockUseAppSelector.mockReturnValue(mockStore);
+
+    const { getByText } = render(<Toast />);
+    const toastElement = getByText(mockStore.text);
+
+    expect(toastElement).toHaveTextContent(mockStore.text);
+  });
+
+  it('トーストを表示しない。', async () => {
+    const mockStore = {
+      displayed: false,
+      text: 'test',
+    };
+    mockUseAppSelector.mockReturnValue(mockStore);
+
+    const { queryByText } = render(<Toast />);
+    const toastElement = queryByText(mockStore.text);
+
+    expect(toastElement).toBeNull();
+  });
+});

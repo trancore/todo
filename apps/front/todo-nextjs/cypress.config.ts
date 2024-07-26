203107e32ebd29bb7c860a56017272980570862a
trancore <ki.program0915@gmail.com>
2024-07-27 02:09:00 +0900

    feature: implement test in first access

--- apps/front/todo-nextjs/cypress.config.ts
+++ apps/front/todo-nextjs/cypress.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    env: {
+      BASE_URL: 'http://localhost:3000',
+    },
     specPattern: ['test/__tests__/e2e/**/*.cy.{ts,tsx}'],
     setupNodeEvents(on, config) {
       config.baseUrl = 'http://localhost:3000';
-
       return config;
     },
   },

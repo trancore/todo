5444374aa958a233d9793ecd06c897760caa0e9e
trancore <ki.program0915@gmail.com>
2024-08-10 00:45:11 +0900

    feature: implement etoe test in completed

---
+++ apps/front/todo-nextjs/test/__tests__/e2e/scenario/08-completed.ts
@@ -0,0 +1,11 @@
+﻿export {};
+
+describe('トップ画面からTODOを編集する。', () => {
+  it('ログイン状態になる', () => {
+    cy.getAllCookies().then((cookies) => {
+      if (!cookies || (cookies && cookies.length < 3)) {
+        cy.signin();
+      }
+    });
+  });
+});

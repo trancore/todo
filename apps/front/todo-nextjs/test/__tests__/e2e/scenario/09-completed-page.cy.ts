24ccced5873c71b4d39a475e6dfd1203cab87d42
trancore <ki.program0915@gmail.com>
2024-08-15 22:23:46 +0900

    feature: implement etoe test in completed page

---
+++ apps/front/todo-nextjs/test/__tests__/e2e/scenario/09-completed-page.cy.ts
@@ -0,0 +1,23 @@
+﻿describe('TODO完了画面にアクセスする。', () => {
+  it('ログイン状態になる', () => {
+    cy.getAllCookies().then((cookies) => {
+      if (!cookies || (cookies && cookies.length < 3)) {
+        cy.signin();
+      }
+    });
+  });
+
+  it('トップ画面に遷移する。', () => {
+    cy.visit('/completed');
+
+    cy.url().should('equal', `${Cypress.env('BASE_URL')}/completed`);
+  });
+
+  it('トップ画面でAPIをコールする。', () => {
+    cy.intercept(`${Cypress.env('API_SERVER_URL')}/todos?status=DONE`).as(
+      'get_todos',
+    );
+
+    cy.wait('@get_todos').its('response.statusCode').should('eq', 304);
+  });
+});

7e846c47da98ef2a0442a58158a5a9137ead07cc
trancore <ki.program0915@gmail.com>
2024-08-21 23:29:53 +0900

    feature: set next-intl settings

---
+++ apps/front/todo-nextjs/src/i18n.ts
@@ -0,0 +1,10 @@
+﻿import { getRequestConfig } from 'next-intl/server';
+
+export default getRequestConfig(async ({ locale }) => {
+  const currentLocale = locale || 'ja';
+
+  return {
+    locale,
+    messages: (await import(`~/messages/${currentLocale}.json`)).default,
+  };
+});

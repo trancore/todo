ce6a2a727bac12771363444652f682b476bb1e71
trancore <ki.program0915@gmail.com>
2024-09-07 00:47:59 +0900

    feature: implement Seo component

---
+++ apps/front/todo-nextjs/src/components/container/Seo/Seo.tsx
@@ -0,0 +1,22 @@
+﻿import Head from 'next/head';
+
+import SeoPresentational from '~/components/presentational/Seo/Seo';
+
+type Props = {
+  children: JSX.Element;
+  title?: string;
+  description?: string;
+};
+
+export default function Seo(props: Props) {
+  const { title: titleString, description } = props;
+  return (
+    <>
+      <Head>
+        <title>{titleString}</title>
+        <meta name="description" content={description} key="description" />
+      </Head>
+      <SeoPresentational>{props.children}</SeoPresentational>
+    </>
+  );
+}

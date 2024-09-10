d340fa4e7ad9d84738b977ac83c1046c28d2c386
trancore <ki.program0915@gmail.com>
2024-09-10 23:00:28 +0900

    feature: implement ogp props in Seo Component

--- apps/front/todo-nextjs/src/components/container/Seo/Seo.tsx
+++ apps/front/todo-nextjs/src/components/container/Seo/Seo.tsx
@@ -4,11 +4,21 @@ import SeoPresentational from '~/components/presentational/Seo/Seo';
 
 type Props = {
   children: JSX.Element;
-  metadata?: { title?: string; description?: string };
+  metadata?: {
+    title: string;
+    description: string;
+  };
+  ogp?: {
+    title: string;
+    description: string;
+    type?: string;
+    image?: string;
+    url?: string;
+  };
 };
 
 export default function Seo(props: Props) {
-  const { metadata } = props;
+  const { metadata, ogp } = props;
   return (
     <>
       <Head>
@@ -18,6 +28,11 @@ export default function Seo(props: Props) {
           content={metadata?.description}
           key="description"
         />
+        <meta property="og:title" content={ogp?.title} />
+        <meta property="og:description" content={ogp?.description} />
+        <meta property="og:type" content={ogp?.type} />
+        <meta property="og:image" content={ogp?.image} />
+        <meta property="og:url" content={ogp?.url} />
       </Head>
       <SeoPresentational>{props.children}</SeoPresentational>
     </>

9fc49a3a35107315d47f3268ac0078800d9c8271
trancore <ki.program0915@gmail.com>
2024-09-18 23:32:28 +0900

    feature: add formatter by eslint stylistic

--- apps/front/todo-nuxtjs/nuxt.config.ts
+++ apps/front/todo-nuxtjs/nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint'],
+  eslint: {
+    config: {
+      stylistic: {
+        semi: true,
+        quotes: 'single',
+        commaDangle: 'always',
+        indent: 2,
+      },
+    },
+  },
 })

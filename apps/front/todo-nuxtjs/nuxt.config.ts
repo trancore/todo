62a644317059dc44d7f068a9a1908c2214b4a91b
trancore <ki.program0915@gmail.com>
2024-09-20 00:05:25 +0900

    feature: refactoring

--- apps/front/todo-nuxtjs/nuxt.config.ts
+++ apps/front/todo-nuxtjs/nuxt.config.ts
@@ -1,11 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true,
+  },
   srcDir: '/src',
   devServer: {
     port: 3001,
   },
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint',],
   eslint: {
     config: {
       stylistic: {
@@ -16,4 +18,4 @@ export default defineNuxtConfig({
       },
     },
   },
-})
+},);

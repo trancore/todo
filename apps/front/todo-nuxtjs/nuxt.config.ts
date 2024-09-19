5b3628342adca0b775023271a98c3dc4232ba025
trancore <ki.program0915@gmail.com>
2024-09-19 23:27:30 +0900

    feature: update srcDir

--- apps/front/todo-nuxtjs/nuxt.config.ts
+++ apps/front/todo-nuxtjs/nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  srcDir: '/src',
+  devServer: {
+    port: 3001,
+  },
   modules: ['@nuxt/eslint'],
   eslint: {
     config: {

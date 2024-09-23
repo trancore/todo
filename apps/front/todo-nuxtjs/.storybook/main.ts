0fe587529d9503e3c68d1452496fef91e0e7c226
trancore <ki.program0915@gmail.com>
2024-09-23 13:15:49 +0900

    feature: add storybook

---
+++ apps/front/todo-nuxtjs/.storybook/main.ts
@@ -0,0 +1,33 @@
+import type { StorybookConfig } from "@storybook/vue3-vite";
+import path from "path";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.stories.ts"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/vue3-vite",
+    options: {
+      docgen: "vue-component-meta",
+    },
+  },
+  staticDirs: ["../src/public"],
+  docs: {
+    autodocs: "tag",
+  },
+  viteFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve?.alias,
+        "~": path.resolve(__dirname, "../src"),
+      };
+    }
+    return config;
+  },
+};
+export default config;
---
+++ apps/front/todo-nuxtjs/vite.config.mjs
@@ -0,0 +1,7 @@
+﻿import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+});

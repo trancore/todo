f71db6624d7752bb862048da9367a0c58922415c
trancore <ki.program0915@gmail.com>
2024-09-23 13:18:13 +0900

    feature: remove test code

--- apps/front/todo-nuxtjs/src/pages/index.vue
+++ apps/front/todo-nuxtjs/src/pages/index.vue
@@ -1,17 +1,5 @@
-<script setup lang="ts">
-import Button from "~/components/Button/Button.vue";
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    <Button
-      :text="'test'"
-      :width="500"
-      :onClick="
-        () => {
-          console.log('Click!!!');
-        }
-      "
-    />
-  </div>
+  <div></div>
 </template>

3d0e82e9fa677574cc1e5994c39acd0f3255d5de
trancore <ki.program0915@gmail.com>
2024-09-24 22:40:05 +0900

    feature: add Error component

---
+++ apps/front/todo-nuxtjs/src/components/Error/Error.vue
@@ -0,0 +1,21 @@
+﻿<script setup lang="ts">
+type Props = {
+  displayed: boolean;
+  text?: string;
+};
+
+const { displayed, text } = defineProps<Props>();
+</script>
+
+<template>
+  <p v-if="displayed" class="error-text">{{ text }}</p>
+</template>
+
+<style lang="scss" scoped>
+.error-text {
+  margin: 0;
+  padding: 24px;
+  background-color: pink;
+  color: red;
+}
+</style>
---
+++ apps/front/todo-nuxtjs/src/stories/Error/Error.stories.ts
@@ -0,0 +1,22 @@
+﻿import type { Meta, StoryObj } from "@storybook/vue3";
+import Error from "~/components/Error/Error.vue";
+
+const meta: Meta<typeof Error> = {
+  title: "Error/Error",
+  component: Error,
+  tags: ["autodocs"],
+  // Add your own control here
+};
+export default meta;
+
+type Story = StoryObj<typeof Error>;
+
+/**
+ * パターン
+ */
+export const Long: Story = {
+  args: {
+    displayed: true,
+    text: "エラーが発生しています。",
+  },
+};

629dba5ccd0a69a8bd0a4d74a835d3fa1ae987e9
trancore <ki.program0915@gmail.com>
2024-09-25 21:35:38 +0900

    feature: refactorig

--- apps/front/todo-nuxtjs/.storybook/main.ts
+++ apps/front/todo-nuxtjs/.storybook/main.ts
@@ -1,30 +1,30 @@
-import type { StorybookConfig } from "@storybook/vue3-vite";
-import path from "path";
+import type { StorybookConfig } from '@storybook/vue3-vite';
+import path from 'path';
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.stories.ts"],
+  stories: ['../src/**/*.stories.ts'],
   addons: [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
+    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
   ],
   framework: {
-    name: "@storybook/vue3-vite",
+    name: '@storybook/vue3-vite',
     options: {
-      docgen: "vue-component-meta",
+      docgen: 'vue-component-meta',
     },
   },
-  staticDirs: ["../src/public"],
+  staticDirs: ['../src/public'],
   docs: {
-    autodocs: "tag",
+    autodocs: 'tag',
   },
   viteFinal: async (config) => {
     if (config.resolve) {
       config.resolve.alias = {
         ...config.resolve?.alias,
-        "~": path.resolve(__dirname, "../src"),
+        '~': path.resolve(__dirname, '../src'),
       };
     }
     return config;
--- apps/front/todo-nuxtjs/src/stories/Form/Form.stories.ts
+++ apps/front/todo-nuxtjs/src/stories/Form/Form.stories.ts
@@ -1,12 +1,13 @@
-﻿import type { Meta, StoryObj } from "@storybook/vue3";
-import Form from "~/components/Form/Form.vue";
+﻿import type { Meta, StoryObj } from '@storybook/vue3';
+import Form from '~/components/Form/Form.vue';
 
 const meta: Meta<typeof Form> = {
-  title: "Form/Form",
+  title: 'Form/Form',
   component: Form,
-  tags: ["autodocs"],
+  tags: ['autodocs'],
   // Add your own control here
 };
 export default meta;
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 type Story = StoryObj<typeof Form>;

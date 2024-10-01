c9589bc40b140924472668ab5757c90ff3fc597e
trancore <ki.program0915@gmail.com>
2024-10-01 22:50:43 +0900

    feature: add TextArea component

---
+++ apps/front/todo-nuxtjs/src/components/Form/TextArea.vue
@@ -0,0 +1,42 @@
+﻿<script setup lang="ts">
+type Props = {
+  labelName: string;
+  errorMessage: string | undefined;
+  placeholder?: string;
+};
+
+defineProps<Props>();
+</script>
+
+<template>
+  <div class="textarea">
+    <label htmlFor="textarea">{{ labelName }}</label>
+    <textarea id="textarea" :rows="10" :placeholder="placeholder" />
+    <p id="textarea-error-message">{{ errorMessage }}</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.textarea {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+
+  label {
+    display: block;
+    margin-bottom: 6px;
+  }
+
+  textarea {
+    width: 100%;
+    box-sizing: border-box;
+    border: 2px solid #000;
+    padding: 8px 10px;
+  }
+
+  p {
+    margin-left: 10px;
+    color: #f00;
+  }
+}
+</style>

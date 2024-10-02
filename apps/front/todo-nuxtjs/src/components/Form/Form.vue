e04295fc17a1eb68e415d4e95d89ad0f9163761d
trancore <ki.program0915@gmail.com>
2024-10-02 22:47:16 +0900

    feature: refactoring css in form component

--- apps/front/todo-nuxtjs/src/components/Form/Form.vue
+++ apps/front/todo-nuxtjs/src/components/Form/Form.vue
@@ -11,35 +11,3 @@ const { submit } = defineProps<Props>();
     <slot />
   </form>
 </template>
-
-<style lang="scss" scoped>
-.form {
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-
-  label {
-    display: block;
-    margin-bottom: 6px;
-  }
-
-  input {
-    width: 100%;
-    box-sizing: content-box;
-    border: 2px solid #000;
-    padding: 8px 10px;
-  }
-
-  textarea {
-    width: 100%;
-    box-sizing: content-box;
-    border: 2px solid #000;
-    padding: 8px 10px;
-  }
-
-  p {
-    margin-left: 10px;
-    color: #f00;
-  }
-}
-</style>

dc4ec4e2cdccc9a3a4aa0367e985e6ec65606d23
trancore <ki.program0915@gmail.com>
2024-10-02 22:36:03 +0900

    feature: add Date component

---
+++ apps/front/todo-nuxtjs/src/components/Form/Date.vue
@@ -0,0 +1,41 @@
+﻿<script setup lang="ts">
+type Props = {
+  labelName: string;
+  errorMessage: string | undefined;
+};
+
+defineProps<Props>();
+</script>
+
+<template>
+  <div class="date">
+    <label htmlFor="date">{{ labelName }}</label>
+    <input id="date" type="date" />
+    <p id="date-error-message">{{ errorMessage }}</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.date {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+
+  label {
+    display: block;
+    margin-bottom: 6px;
+  }
+
+  input {
+    width: 100%;
+    box-sizing: border-box;
+    border: 2px solid #000;
+    padding: 8px 10px;
+  }
+
+  p {
+    margin-left: 10px;
+    color: #f00;
+  }
+}
+</style>
---
+++ apps/front/todo-nuxtjs/src/stories/Form/Date.stories.ts
@@ -0,0 +1,23 @@
+﻿import type { Meta, StoryObj } from '@storybook/vue3';
+
+import Date from '~/components/Form/Date.vue';
+
+const meta: Meta<typeof Date> = {
+  title: 'Form/Date',
+  component: Date,
+  tags: ['autodocs'],
+  // Add your own control here
+};
+export default meta;
+
+type Story = StoryObj<typeof Date>;
+
+/**
+ * パターン
+ */
+export const TodoDeadlineAt: Story = {
+  args: {
+    labelName: '期限',
+    errorMessage: 'エラーメッセージが表示されます。',
+  },
+};

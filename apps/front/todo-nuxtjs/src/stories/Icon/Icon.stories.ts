718cb6e46103c5a3674f4f4129c1c778dce8a60e
trancore <ki.program0915@gmail.com>
2024-10-04 00:33:16 +0900

    feature: add Icon svg

---
+++ apps/front/todo-nuxtjs/src/stories/Icon/Icon.stories.ts
@@ -0,0 +1,34 @@
+﻿import type { Meta, StoryObj } from '@storybook/vue3';
+
+import Icon from '~/components/Icon/Icon.vue';
+
+const meta: Meta<typeof Icon> = {
+  title: 'Icon/Icon',
+  component: Icon,
+  tags: ['autodocs'],
+  // Add your own control here
+};
+export default meta;
+
+type Story = StoryObj<typeof Icon>;
+
+/**
+ * パターン
+ */
+export const Check32Black: Story = {
+  args: {
+    name: 'Check',
+    size: 32,
+    color: '#000000',
+    clickIcon: () => {},
+  },
+};
+
+export const Uncheck64Red: Story = {
+  args: {
+    name: 'Uncheck',
+    size: 64,
+    color: '#FF0000',
+    clickIcon: () => {},
+  },
+};

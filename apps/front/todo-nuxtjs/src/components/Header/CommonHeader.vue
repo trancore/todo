5aa88d4ad4f6f0badb3adb9ce9b56c613f53a48d
trancore <ki.program0915@gmail.com>
2024-10-06 01:39:26 +0900

    feature: add Header component

---
+++ apps/front/todo-nuxtjs/src/components/Header/CommonHeader.vue
@@ -0,0 +1,39 @@
+﻿<script setup lang="ts">
+import { PAGE_PATH } from '~/constants/page';
+
+type Props = {
+  isSignin: boolean;
+  hasPlusIcon: boolean;
+  onClickMenuIcon: () => void;
+  onClickPlusIcon: () => void;
+};
+
+defineProps<Props>();
+</script>
+
+<template>
+  <header class="header">
+    <template v-if="isSignin">
+      <Icon name="Menu" color="#000000" :size="64" />
+      <NuxtLink :to="PAGE_PATH.TOP">
+        <Icon name="UserCircle" color="#000000" :size="64" />
+      </NuxtLink>
+      <Icon v-if="hasPlusIcon" name="Plus" color="#000000" :size="64" />
+    </template>
+    <h1 v-else class="title">todo</h1>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  padding: 18px 21px;
+  background-color: #8f8f8f;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.title {
+  margin: auto;
+}
+</style>

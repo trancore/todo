ca7499150036fac6835ac26998c97ea16fdea190
trancore <ki.program0915@gmail.com>
2024-10-07 21:33:21 +0900

    feature: add TodoDetail Modal component

---
+++ apps/front/todo-nuxtjs/src/stories/Modal/TodoDetail.stories.ts
@@ -0,0 +1,37 @@
+﻿import type { Meta, StoryObj } from '@storybook/vue3';
+
+import TodoDetail from '~/components/Modal/TodoDetail.vue';
+
+const meta: Meta<typeof TodoDetail> = {
+  title: 'Modal/TodoDetail',
+  component: TodoDetail,
+  tags: ['autodocs'],
+  // Add your own control here
+};
+export default meta;
+
+type Story = StoryObj<typeof TodoDetail>;
+
+/**
+ * パターン
+ */
+export const TodoDetailModal: Story = {
+  args: {
+    todo: {
+      title: 'やることやることやること',
+      description:
+        'やることやることやることやることやることやることやることやることやることやることやることやることやることやることやることやることやることやることやることやることやることやることやることやること',
+      deadlineAt: '2024/01/01',
+    },
+    locateCompleted: false,
+    disabled: {
+      completedButtonDisabled: false,
+      deletedButtonDisabled: false,
+    },
+    event: {
+      clickCompletedButton: () => {},
+      clickEditButton: () => {},
+      clickDeleteButton: () => {},
+    },
+  },
+};

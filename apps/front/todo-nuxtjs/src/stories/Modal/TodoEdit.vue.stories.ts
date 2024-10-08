aa1a15df6fe14316548aae47c708cf7f1c8ebe7e
trancore <ki.program0915@gmail.com>
2024-10-08 23:58:39 +0900

    feature: add TodoEditModal component

---
+++ apps/front/todo-nuxtjs/src/stories/Modal/TodoEdit.vue.stories.ts
@@ -0,0 +1,29 @@
+﻿import type { Meta, StoryObj } from '@storybook/vue3';
+
+import TodoEdit from '~/components/Modal/TodoEdit.vue';
+
+const meta: Meta<typeof TodoEdit> = {
+  title: 'Modal/TodoEdit',
+  component: TodoEdit,
+  tags: ['autodocs'],
+  // Add your own control here
+};
+export default meta;
+
+type Story = StoryObj<typeof TodoEdit>;
+
+/**
+ * パターン
+ */
+export const TodoEditModal: Story = {
+  args: {
+    todo: {
+      title: 'やることやることやること',
+      description:
+        'やることやることやることやることやることやることやることやることやることやることやることやることやることやることやることやることやることやることやることやることやることやることやることやること',
+    },
+    event: {
+      onClickEdit: () => {},
+    },
+  },
+};

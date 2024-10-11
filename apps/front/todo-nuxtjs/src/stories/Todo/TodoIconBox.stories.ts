e13bf663e12e5f6b282ec2e11c171d3ba44b839b
trancore <ki.program0915@gmail.com>
2024-10-11 23:38:00 +0900

    feature: TodoIconBox component

---
+++ apps/front/todo-nuxtjs/src/stories/Todo/TodoIconBox.stories.ts
@@ -0,0 +1,37 @@
+﻿import type { Meta, StoryObj } from '@storybook/vue3';
+
+import TodoIconBox from '~/components/Todo/TodoIconBox.vue';
+
+const meta: Meta<typeof TodoIconBox> = {
+  title: 'Todo/TodoIconBox',
+  component: TodoIconBox,
+  tags: ['autodocs'],
+  // Add your own control here
+};
+export default meta;
+
+type Story = StoryObj<typeof TodoIconBox>;
+
+/**
+ * パターン
+ */
+export const Default: Story = {
+  args: {
+    uncheck: {
+      has: true,
+      click: () => {},
+    },
+    check: {
+      has: true,
+      click: () => {},
+    },
+    squareEdit: {
+      has: true,
+      click: () => {},
+    },
+    trashCan: {
+      has: true,
+      click: () => {},
+    },
+  },
+};

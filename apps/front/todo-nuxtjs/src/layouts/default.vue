f53a9fd176e8addb45618de7bfead90fe0cf6cbf
trancore <ki.program0915@gmail.com>
2024-10-12 23:26:02 +0900

    feature: add layout

---
+++ apps/front/todo-nuxtjs/src/layouts/default.vue
@@ -0,0 +1,31 @@
+﻿<script setup lang="ts">
+type Props = {
+  openedMenu: boolean;
+  openedTodoDetailModal: boolean;
+  openedTodoEditModal: boolean;
+};
+
+const { openedMenu, openedTodoDetailModal, openedTodoEditModal } =
+  defineProps<Props>();
+</script>
+
+<template>
+  <div>
+    <Menu v-if="openedMenu" />
+    <TodoDetail v-if="openedTodoDetailModal" />
+    <TodoEdit v-if="openedTodoEditModal" />
+    <HeaderCommonHeader />
+    <Error :displayed="true" text="error" />
+    <div class="layout">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.layout {
+  margin: 0 auto;
+  padding: 44px 15px;
+  max-width: 512px;
+}
+</style>

a6e044eb159aff3f170f5d64734400a83cf8f6d1
trancore <ki.program0915@gmail.com>
2026-02-14 23:48:16 +0900

    feature: update project name

---
+++ apps/front/todo-sveltejs_v5/src/lib/paraglide/messages/_index.js
@@ -0,0 +1,5 @@
+/* eslint-disable */
+import { getLocale, trackMessageCall, experimentalMiddlewareLocaleSplitting, isServer, experimentalStaticLocale } from "../runtime.js"
+/** @typedef {import('../runtime.js').LocalizedString} LocalizedString */
+import * as __en from "./en.js"
+import * as __ja from "./ja.js"

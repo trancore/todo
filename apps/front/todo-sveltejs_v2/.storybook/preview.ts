e29b609dec8d0428d2ad2ed2802bb04cc5af858a
trancore <ki.program0915@gmail.com>
2026-02-19 21:59:01 +0900

    feature: add tailwind.css

--- apps/front/todo-sveltejs_v2/.storybook/preview.ts
+++ apps/front/todo-sveltejs_v2/.storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from '@storybook/sveltekit';
+import '../src/lib/assets/css/tailwind.css';
 
 const preview: Preview = {
 	parameters: {
@@ -8,14 +9,14 @@ const preview: Preview = {
 				date: /Date$/i
 			}
 		},
-
 		a11y: {
 			// 'todo' - show a11y violations in the test UI only
 			// 'error' - fail CI on a11y violations
 			// 'off' - skip a11y checks entirely
 			test: 'todo'
 		}
-	}
+	},
+	tags: ['autodocs']
 };
 
 export default preview;
--- apps/front/todo-sveltejs_v2/src/routes/+layout.svelte
+++ apps/front/todo-sveltejs_v2/src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import { locales, localizeHref } from '$lib/paraglide/runtime';
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import '$lib/assets/css/tailwind.css';
 
 	let { children } = $props();
 </script>

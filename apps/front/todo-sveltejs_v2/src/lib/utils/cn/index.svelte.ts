04ab0aae26c508743c4843a149bbf9b32f040b81
trancore <ki.program0915@gmail.com>
2026-02-19 23:32:44 +0900

    feature: add cn utils

---
+++ apps/front/todo-sveltejs_v2/src/lib/utils/cn/index.svelte.ts
@@ -0,0 +1,20 @@
+﻿import clsx, { type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+/**
+ * クラス名管理カスタムヘルパー関数。\
+ * 引数の順番に応じてグルーピングされたスタイルが定義される。
+ *
+ * 第1引数：カスタムクラス\
+ * 第2引数：余白、大きさ\
+ * 第3引数：配置\
+ * 第4引数：フォント\
+ * 第5引数：装飾\
+ * 末端引数：条件分岐によって変わるスタイル
+ *
+ * @param inputs クラス名
+ * @returns マージされたクラス名
+ */
+export const cn = (...inputs: ClassValue[]) => {
+	return twMerge(clsx(inputs));
+};

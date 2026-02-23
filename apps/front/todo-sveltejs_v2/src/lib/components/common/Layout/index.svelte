05cdc8ea2ca088db8b16c73c97f8115b80967ada
trancore <ki.program0915@gmail.com>
2026-02-23 20:09:38 +0900

    feature: add Layout component

---
+++ apps/front/todo-sveltejs_v2/src/lib/components/common/Layout/index.svelte
@@ -0,0 +1,20 @@
+﻿<script lang="ts">
+	import Header from '$lib/components/ui/Header/index.svelte';
+	import { cn } from '$lib/utils/cn/index.svelte';
+	import type { Snippet } from 'svelte';
+
+	interface Props {
+		content: Snippet;
+	}
+
+	let { content }: Props = $props();
+</script>
+
+<div class={cn('min-h-screen', 'flex flex-col')}>
+	<Header />
+	<div class={cn('flex-1')}>
+		<main class={cn('mx-auto max-w-180')}>
+			{@render content()}
+		</main>
+	</div>
+</div>

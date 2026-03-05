e4486469a6d80139c1916b34501a331f72690ebc
trancore <ki.program0915@gmail.com>
2026-03-05 22:39:29 +0900

    feature: add Toast component

---
+++ apps/front/todo-sveltejs_v2/src/lib/components/ui/Toast/index.svelte
@@ -0,0 +1,13 @@
+﻿<script lang="ts">
+	import { cn } from '$lib/utils/cn/index.svelte';
+
+	interface Props {
+		message: string;
+	}
+
+	let { message }: Props = $props();
+</script>
+
+<div class={cn('px-6 py-3', 'flex items-center justify-center', 'border-2 border-red-400')}>
+	<p class={cn('font-bold')}>{message}</p>
+</div>

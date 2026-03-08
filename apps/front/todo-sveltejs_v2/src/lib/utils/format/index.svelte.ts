82b92872692faa4188f7c546a4a1864474013ca5
trancore <ki.program0915@gmail.com>
2026-03-08 21:41:54 +0900

    feature: add format utils

---
+++ apps/front/todo-sveltejs_v2/src/lib/utils/format/index.svelte.ts
@@ -0,0 +1,12 @@
+﻿/**
+ * 日付を「YYYY/MM/DD」形式でフォーマットする関数
+ * @param date フォーマットする日付
+ * @return フォーマットされた日付文字列
+ */
+export const formatYYYYMMHH = (date: Date) => {
+	return new Intl.DateTimeFormat('ja-JP', {
+		year: 'numeric',
+		month: '2-digit',
+		day: '2-digit'
+	}).format(date);
+};

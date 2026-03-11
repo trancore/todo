d869f78fad627d4cc9f2ecc0103e241a75267b84
trancore <ki.program0915@gmail.com>
2026-03-11 19:59:54 +0900

    feature: add Todo component at wip

---
+++ apps/front/todo-sveltejs_v2/src/lib/components/ui/Todo/index.stories.svelte
@@ -0,0 +1,59 @@
+﻿<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+
+	import Todo from './index.svelte';
+
+	const { Story } = defineMeta({
+		component: Todo,
+		title: 'ui/Todo',
+		parameters: {
+			layout: 'centered'
+		}
+	});
+</script>
+
+<Story
+	name="seven_days_or_more"
+	args={{
+		title: 'titletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitle',
+		description:
+			'descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription',
+		expired: new Date()
+	}}
+/>
+<Story
+	name="less_than_seven_days"
+	args={{
+		title: 'titletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitle',
+		description:
+			'descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription',
+		expired: new Date()
+	}}
+/>
+<Story
+	name="three_days_ago"
+	args={{
+		title: 'titletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitle',
+		description:
+			'descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription',
+		expired: new Date()
+	}}
+/>
+<Story
+	name="on_the_day"
+	args={{
+		title: 'titletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitle',
+		description:
+			'descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription',
+		expired: new Date()
+	}}
+/>
+<Story
+	name="expired"
+	args={{
+		title: 'titletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitle',
+		description:
+			'descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription',
+		expired: new Date()
+	}}
+/>
---
+++ apps/front/todo-sveltejs_v2/src/lib/components/ui/Todo/index.svelte
@@ -0,0 +1,40 @@
+﻿<script lang="ts">
+	import { cn } from '$lib/utils/cn/index.svelte';
+	import { formatYYYYMMHH } from '$lib/utils/format/index.svelte';
+
+	interface Props {
+		title: string;
+		description: string | undefined;
+		expired: Date;
+	}
+
+	let { title, description, expired }: Props = $props();
+</script>
+
+<div class={cn('w-full', 'flex flex-col')}>
+	<div
+		class={cn(
+			'gap-2 px-12 py-8',
+			'flex flex-col',
+			'break-all',
+			'overflow-hidden rounded-full border-3 border-gray-400'
+		)}
+	>
+		<p class={cn('line-clamp-1 text-2xl font-bold')}>
+			{title}
+		</p>
+		{#if description !== undefined}
+			<p class={cn('line-clamp-2')}>
+				{description}
+			</p>
+		{/if}
+	</div>
+	<div>
+		<p>{formatYYYYMMHH(expired)}</p>
+		<div>
+			<div>Icon</div>
+			<div>Icon</div>
+			<div>Icon</div>
+		</div>
+	</div>
+</div>

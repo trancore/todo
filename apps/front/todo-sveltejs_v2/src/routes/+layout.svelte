0c07047390235f7d13664e8da19bd161059baf87
trancore <ki.program0915@gmail.com>
2026-04-03 23:39:28 +0900

    feature: update top page template

--- apps/front/todo-sveltejs_v2/src/routes/+layout.svelte
+++ apps/front/todo-sveltejs_v2/src/routes/+layout.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 	import '$lib/assets/css/tailwind.css';
 
+	import Header from '$lib/components/ui/Header/index.svelte';
+	import { cn } from '$lib/utils/cn/index.svelte';
+
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" /></svelte:head>
 
-{@render children()}
+<Header />
+<main class={cn('mx-auto my-12 max-w-180', 'flex justify-center')}>
+	{@render children()}
+</main>
